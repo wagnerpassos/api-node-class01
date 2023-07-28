051beadb80fce1f42fc17a6d5ff44854e2a9f27f
Wagner Passos <wagner_passos@hotmail.com>
2023-07-28 19:22:48 -0300

    refactored server.js and added nodemon on package.json dependencies

--- src/server.js
+++ src/server.js
@@ -5,6 +5,16 @@ const app = express();
 // ---- GLOBALs
 const PORT = '3000';
 
+app.get('/:id', (req, res) => {
+    const {id} = req.params;
+
+    res.send(`your id is ${id}`);
+});
+
+app.get('/', (req, res) => {
+    res.send("HELLO WORLD!");
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
